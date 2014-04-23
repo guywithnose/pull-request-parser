fff3c62765edeff416e1cabf1decc58aa3eb6152
Robert Bittle <guywithnose@gmail.com>
2014-04-23 09:03:02 -0400

    Open PRs in new tab

--- js/main.js
+++ js/main.js
@@ -138,7 +138,7 @@
   }
 
   function buildRow(pullRequest) {
-    var row = '<td><a href="' + pullRequest.html_url + '">' + pullRequest.number + '</a></td><td title="' + approvalTitle(pullRequest) + '">' +
+    var row = '<td><a href="' + pullRequest.html_url + '" target="_blank">' + pullRequest.number + '</a></td><td title="' + approvalTitle(pullRequest) + '">' +
       pullRequest.numApprovals + '</td><td>' + (pullRequest.isRebased ? 'Y' : 'N') + '</td><td>' +
       (pullRequest.state == 'success' ? 'Y' : pullRequest.state == 'none' || pullRequest.state == 'pending' ? '?' : 'N') + '</td><td>' +
       (!pullRequest.iHaveApproved && !pullRequest.iAmOwner ? 'Y' : 'N') + '</td><td>' +
