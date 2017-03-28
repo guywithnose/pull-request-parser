f4223d0833c74456292cd5fa2fa4c77c372585a7
Robert Bittle <guywithnose@gmail.com>
2017-03-28 14:28:23 -0400

    Make user the primary parameter and owner the alias

--- prp/commands.go
+++ prp/commands.go
@@ -50,7 +50,7 @@ var Commands = []cli.Command{
 		BashComplete: command.CompleteParse,
 		Flags: []cli.Flag{
 			cli.StringFlag{
-				Name:  "owner, user, o",
+				Name:  "user, owner, u",
 				Usage: "Only show pull requests by owner.",
 			},
 			cli.StringSliceFlag{
